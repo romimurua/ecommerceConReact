6d2f768b8bcef4d92b297b4bea1509571d68914e
romimurua <99356832+romimurua@users.noreply.github.com>
2023-04-25 14:22:11 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
   const [ searchValue, setSearchValue ] = useState("");
 
   useEffect (() => {
-    axios.get("https://ecommerce-api-react.herokuapp.com/api/v1/products/categories")
+    axios.get("https://e-commerce-api.academlo.tech/api/v1/products/categories")
       .then(res => setCategories(res.data.data.categories))
   }, [])
 
